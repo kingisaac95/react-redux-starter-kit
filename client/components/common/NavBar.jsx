e91e8bfa62384c3544c1a631164b094278f3bfeb
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2018-01-10 13:26:13 +0100

    (task): update navbar and readme

--- client/components/common/NavBar.jsx
+++ client/components/common/NavBar.jsx
@@ -13,7 +13,7 @@ const NavBar = () => {
               <span className="icon-bar"/>
               <span className="icon-bar"/>
             </button>
-            <Link to="/" className="navbar-brand navbrand-wrapper">Afiora</Link>
+            <Link to="/" className="navbar-brand navbrand-wrapper">React Redux Starter Kit</Link>
           </div>
 
           <div className="collapse navbar-collapse" id="navbar-collapse">
